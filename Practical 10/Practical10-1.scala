6b0a9b39e61791485fdf457e522537ddc51dfed1
Naleeka Kumarasinghe <knaleeka845@gmail.com>
2024-09-09 23:32:15 +0530

    practical 1 done

--- Practical 10/Practical10-1.scala
+++ Practical 10/Practical10-1.scala
@@ -1,44 +1,30 @@
-class Rational(numerator: Int, denominator: Int) {
-  require(denominator != 0, "Denominator cannot be zero")
+class Rational(n: Int, d: Int) {
+  require(d != 0, "Denominator cannot be zero")
 
   // Simplifying numerator and denominator
-  private val gcdValue = Rational.gcd(numerator.abs, denominator.abs)
-  val numer: Int = numerator / gcdValue
-  val denom: Int = denominator / gcdValue
+  private val g = RationalMethod.gcd(n.abs, d.abs)
+  val numer: Int = n / g
+  val denom: Int = d / g
 
-  // Method to negate the rational number
-  def neg: Rational = new Rational(-this.numer, this.denom)
+  // Negation method to return a negated rational number
+  def neg: Rational = RationalMethod.negate(this)
 
-  // Subtraction method
-  def subtract(that: Rational): Rational = {
-    val newNumer = this.numer * that.denom - that.numer * this.denom
-    val newDenom = this.denom * that.denom
-    new Rational(newNumer, newDenom)
-  }
-
-  override def toString: String = s"$numer/$denom"
+  override def toString: String = if (denom == 1) s"$numer" else s"$numer/$denom"
 }
 
-object Rational {
+object RationalMethod {
   // Method to compute the greatest common divisor
   def gcd(a: Int, b: Int): Int = {
     if (b == 0) a else gcd(b, a % b)
   }
+
+  // Method to negate a Rational number
+  def negate(r: Rational): Rational = new Rational(-r.numer, r.denom)
 }
 
-// Usage example with proper structure
 object MainMethod {
   def main(args: Array[String]): Unit = {
-    val rational1 = new Rational(3, 4)
-    val rational2 = new Rational(5, 8)
-    val rational3 = new Rational(2, 7)
-
-    // Calculate rational1 - rational2 - rational3
-    val result = rational1.subtract(rational2).subtract(rational3)
-    println(result)  // Output: 13/56
-
-    // Negate a rational number
-    val negatedRational1 = rational1.neg
-    println(negatedRational1)  // Output: -3/4
+    val x = new Rational(3, 4)
+    println(x.neg)  // Output: -3/4
   }
 }
